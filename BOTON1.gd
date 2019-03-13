9ecc1d2fc90a3fa8c5b75d3748b519f2a2a44633
PaoloBE <budiel420@hotmail.com>
2019-03-13 16:41:18 -0500

    non posso lol

--- BOTON1.gd
+++ BOTON1.gd
@@ -1,8 +1,9 @@
 extends Sprite
 
 var mouseIn = false
-var posiX = self.get_transform().x
-var posiY = self.get_transform().y
+var iniX = self.get_transform().get_origin().round().x
+var iniY = self.get_transform().get_origin().round().y
+var tst = Vector2(50,50)
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -28,10 +29,15 @@ func _on_Area2D_mouse_exited():
 
     
 func _on_Area2D_input_event(viewport, event, shape_idx):
-  print(get_parent().get_node("Teste").get_transform().get_origin().round()," con:",get_viewport().get_mouse_position().round())
+  print(get_viewport().get_mouse_position().round() - get_parent().get_node("Teste").get_transform().origin)
+  #print(iniX,",",iniY," con: ",get_viewport().get_mouse_position().round())
+  #print(get_parent().get_node("Teste").texture.get_size())
+  #print(get_parent().get_node("Teste").get_transform().get_origin().round()," con:",get_viewport().get_mouse_position().round())
   if event is InputEventMouseButton:
     if event.is_pressed() == false:
-	     print("no en: ", event.position)
+         print("no en: ", event.position)
+         if abs(get_viewport().get_mouse_position().round().x - get_parent().get_node("Teste").get_transform().origin.x) >= 25 or abs(get_viewport().get_mouse_position().round().y - get_parent().get_node("Teste").get_transform().origin.y) >= 25:
+            set_position(Vector2(iniX,iniY))
     else:
          print("si en: ", event.position)
   
